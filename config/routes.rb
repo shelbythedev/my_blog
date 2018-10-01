825f3d9b9155c6a6eb32f324190b660064c51a45
Shelby Solomon <shelby@shelbysolomon.net>
2018-10-01 11:17:47 -0500

    static pages and initial stylings

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'pages#home'
+  root to: 'posts#home'
 end
